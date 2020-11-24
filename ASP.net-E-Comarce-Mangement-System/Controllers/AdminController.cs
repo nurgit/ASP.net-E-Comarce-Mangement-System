2d8eedd4f126eacac4f71ae0fd99eeebc7383232
Nur Mohammad <16-33013-3@student.aiub.edu>
2020-11-24 09:54:26 +0600

    p7 add category

--- ASP.net-E-Comarce-Mangement-System/Controllers/AdminController.cs
+++ ASP.net-E-Comarce-Mangement-System/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using ASP.net_E_Comarce_Mangement_System.Models;
 using ASP.net_E_Comarce_Mangement_System.Repository;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,8 +21,33 @@ namespace ASP.net_E_Comarce_Mangement_System.Controllers
 
         public ActionResult Categories()
         {
+            // for category list 
             List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
             return View(allcategories);
+        }
+        public ActionResult AddCategory()
+        {
+            return UpdateCategory(0);
+        }
+
+        public ActionResult UpdateCategory(int categoryId)
+        {
+            CategoryDetail cd;
+            if (categoryId != null)
+            {
+                cd = JsonConvert.DeserializeObject<CategoryDetail>(JsonConvert.SerializeObject(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(categoryId)));
+            }
+            else
+            {
+                cd = new CategoryDetail();
+            }
+            return View("UpdateCategory", cd);
+
+        }
+
+        public ActionResult Product()
+        {
+            return View();
         }
     }
 }
